d7db7f73b53ff5f2a262f6b900efd78b30f1b9e7
Emmanueladeyemi123 <96444111+Emmanueladeyemi123@users.noreply.github.com>
2022-08-11 17:22:17 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 https://teachablemachine.withgoogle.com/models/0MGq-9ya5/
+
+
+Webcam.set({
+    height: 300,
+    width: 400,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+
+function capture() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img src="' + data_uri + '" id="captured_image">'
+    });
+}
+
+console.log("ml5 version is:", ml5.version);
+classifer = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/0MGq-9ya5/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model has loaded");
+}
